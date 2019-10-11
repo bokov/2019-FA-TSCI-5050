073c0f8fbcc0bf01657c1bae0a25981ac790f72b
Alex Bokov <bokov@uthscsa.edu>
2019-10-11 15:11:25 -0500

    patch_191011.R; patch; Small script designed for remote execution directly off github

---
+++ patch_191011.R
@@ -0,0 +1,5 @@
+systemwrapper('wget -N https://raw.githubusercontent.com/bokov/2019-FA-TSCI-5050/update_00/.gitmodules');
+devtools::install_github('bokov/tidbits',ref='integration');
+devtools::install_github('bokov/rio',ref='master');
+clean_slate('library(tidbits)');
+c()

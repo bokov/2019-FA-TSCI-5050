98baf4ec87621062c6a1bfb83fb92841794c1257
Alex Bokov <bokov@uthscsa.edu>
2019-10-11 15:15:37 -0500

    patch_191011.R; bugfix; Cannot rely on systemwrapper() being available, using system

--- patch_191011.R
+++ patch_191011.R
@@ -1,4 +1,4 @@
-systemwrapper('wget -N https://raw.githubusercontent.com/bokov/2019-FA-TSCI-5050/update_00/.gitmodules');
+system('wget -N https://raw.githubusercontent.com/bokov/2019-FA-TSCI-5050/update_00/.gitmodules');
 devtools::install_github('bokov/tidbits',ref='integration');
 devtools::install_github('bokov/rio',ref='master');
 clean_slate('library(tidbits)');

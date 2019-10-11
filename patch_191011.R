0132f7685f5539251490bcffcb68f419854c6819
Alex Bokov <bokov@uthscsa.edu>
2019-10-11 15:34:47 -0500

    patch_191011.R; patch; Automatic retrying

--- patch_191011.R
+++ patch_191011.R
@@ -1,6 +1,9 @@
-system('wget -N https://raw.githubusercontent.com/bokov/2019-FA-TSCI-5050/update_00/.gitmodules');
-devtools::install_github('bokov/tidbits',ref='integration');
-devtools::install_github('bokov/rio',ref='master');
-file.rename('data_characterization.R','.data_characterization.backup');
-clean_slate('library(tidbits)');
+if(!exists('.url')) .url <- 'https://raw.githubusercontent.com/bokov/2019-FA-TSCI-5050/update_00';
+if(!exists('.thisfile')) .thisfile <- 'patch_191011.R';
+if(!exists('.attempt')) .attempt <- 1;
+system(paste0('wget -N ',file.path(.url,'.gitmodules')));
+try(devtools::install_github('bokov/tidbits',ref='integration'));
+try(devtools::install_github('bokov/rio',ref='master'));
+try(file.rename('data_characterization.R','.data_characterization.backup'));
+clean_slate(sprintf('.attempt <- %d; if(!require(tidbits) && .attempt < 3) source("%s%s")',.attempt+1,.url,.thisfile));
 c()

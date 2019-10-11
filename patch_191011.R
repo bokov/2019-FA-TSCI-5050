9aabb62cd4a328dbba77f444a98ede4d9bf76725
Alex Bokov <bokov@uthscsa.edu>
2019-10-11 15:22:36 -0500

    patch_191011.R; tweak; Make a backup of the original data_characterization.R file

--- patch_191011.R
+++ patch_191011.R
@@ -1,5 +1,6 @@
 system('wget -N https://raw.githubusercontent.com/bokov/2019-FA-TSCI-5050/update_00/.gitmodules');
 devtools::install_github('bokov/tidbits',ref='integration');
 devtools::install_github('bokov/rio',ref='master');
+file.rename('data_characterization.R','.data_characterization.backup');
 clean_slate('library(tidbits)');
 c()

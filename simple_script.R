3126c7e084fea2e60f1bf6711edcbe8f63f855a2
Alex Bokov <bokov@uthscsa.edu>
2019-11-06 09:41:19 -0600

    simple_script.R; enhance; Outline for a manual, static script that does not rely on the 'scripts' folder

---
+++ simple_script.R
@@ -0,0 +1,53 @@
+#' ---
+#' title: "Simple R Script"
+#' subtitle: "TSCI 5050: Introduction to Data Science"
+#' author: "Alex F. Bokov"
+#' date: "November 6, 2019"
+#' ---
+
+#' # Load Libraries 
+#' 
+#' Automation exists
+#' 
+#' # Set Variables
+#' 
+#' Automation exists
+#' 
+#' # Load Data
+#' 
+#' Automation exists
+#' 
+#' # Take Random Developmental Sample
+#' 
+#' Manual process
+#' 
+#' # Review Data
+#' 
+#' Manual process
+#' 
+#' # Fix Problems
+#' 
+#' Manual process
+#' 
+#' # Fit Univariate Models
+#' 
+#' Manual process but can be facilitated by `for()` or `sapply()`
+#' 
+#' # Fit a Multivariable Model
+#' 
+#' Manual process
+#' 
+#' # Check Model Assumptions
+#' 
+#' Manual process
+#' 
+#' # Apply Model/s to Hold-Out Data
+#' 
+#' Manual process
+#' 
+#' # Conclusions
+#' 
+#' Finalize above plots and tables. Write up interpretation of results.
+#' 
+#' # Save objects for re-use later
+#' 

f290306bb33d4ff01da5390493dc2bbb9b5448ad
Alex Bokov <bokov@uthscsa.edu>
2019-11-18 16:08:46 -0600

    quickstart.R; deployment; Moved some of the complexity to functions in tidbits, now installs tidbits and dependencies

--- quickstart.R
+++ quickstart.R
@@ -47,105 +47,110 @@ to it.
 .templatepath <- 'https://github.com/bokov/2019-FA-TSCI-5050/archive/ft_simplescript.zip';
 .scriptspath <- 'https://github.com/bokov/ut-template/archive/master.zip';
 .checkenv <- new.env();
-
-unzipto <- function(zipfile,to='.',cleanup=getOption('cleanup',TRUE)){
-  #' Get file listing
-  .checkfiles <- utils::unzip(zipfile,list=TRUE);
-  #' Create directories
-  .ziproot <- basename(.checkfiles$Name[1]);
-  sapply(with(.checkfiles,normalizePath(sub(.ziproot,to,Name[Length==0])
-                                        ,mustWork = FALSE))
-         ,dir.create,recursive=TRUE);
-  #' Unzip the files
-  .copiedfiles <- utils::unzip(zipfile);
-  file.copy(.copiedfiles,normalizePath(sub(.ziproot,to,.copiedfiles)
-                                       ,mustWork = FALSE));
-  if(cleanup) unlink(c(zipfile,.ziproot),recursive = TRUE,force = TRUE);
-}
-
-getbrsub <- function(file='.gitmodules',section='\\[submodule'
-                     ,targetsection='\\[submodule "scripts"'
-                     ,params=c('url','branch')){
-  # read in the modules file
-  .subsraw <- readLines(file);
-  # find the places where it switches to a different submodule, or ends
-  .subssections <- c(grep(section,.subsraw),length(.subsraw)+1);
-  # find the start of the submodule of interest
-  .subsscripts <- grep(targetsection,.subsraw);
-  # find the start and end of just the body of that submodule section
-  .subsextent <- .subssections[match(.subsscripts,.subssections)+0:1] + 
-    c(1,-1);
-  # find the actual body
-  .substarget <- .subsraw[seq(.subsextent[1],.subsextent[2])];
-  sapply(params,function(xx) if(any(oo<-grepl(xx,.substarget))){
-    gsub(' ','',strsplit(tail(.substarget[oo],1),'=')[[1]][2])} else '');
-}
-
-#' 
 #' TODO: parse own URL
 #' 
+#' Install needed libraries
+message('Installing needed packages and their dependencies');
+if(!require('devtools')){
+  install.packages('devtools',dependencies=TRUE
+                   ,repos=getOption('repos','https://cran.rstudio.com'))};
+devtools::install_github('bokov/trailR',ref='integration'); library(trailR);
+devtools::install_github('bokov/tidbits',ref='integration'); library(tidbits);
+devtools::install_github('bokov/rio',ref='master'); library(rio);
+instrequire('usethis');
+
+#' 
 #' Copy down the latest version of the specified branch
-utils::download.file(.templatepath,'.temp.zip',quiet=TRUE);
-unzipto('.temp.zip','.');
+.ztemp0 <- usethis::use_zip(.templatepath,'.',cleanup = TRUE);
+#' Merge into current directory, with backups
+mergedirs(.ztemp0);
+#' Get info about the scripts submodule
+.scriptsinfo <- getkeyval(
+  filesections('.gitmodules',sectionrxp = '\\[.*submodule'
+               ,targetrxp = '\\[.*submodule .*scripts.*\\]')[[1]]
+  ,c('path','url','branch'));
+#' Download the appropriate version of the submodule
+.ztemp1 <- usethis::use_zip(
+  with(.scriptsinfo,paste0(url,'/archive/',c(branch,'master')[1],'.zip'))
+  ,'.',cleanup = TRUE);
+#' Rename it to its standard local name
+unlink(.scriptsinfo$path,recursive = TRUE,force = TRUE);
+file.rename(.ztemp1,.scriptsinfo$path);
+#' For setting inputdata
+#' 
+.userconfigdone <- FALSE;
+#' While !.userconfigdone
+#' Select a data file to include
+#' These are the data-files you selected so far and the variables into which
+#' they will be read. What do you wish to do next?
+#' [0. Re-display this menu]
+#' 1. Start over (remove all the files in this list)
+#' 2. Add more files
+#' 3. Edit one of the variable names
+#' 4. Done
+#' 
+#' If edit: 
+#' Which variable do you wish to rename 
+#' (hit 0 or XX to go back to previous menu)
+#' 
+#' If selected a variable:
+#' Please note that your variable names might get modified for compatibility 
+#' with R and uniqueness. Hit 'enter' if you no longer wish to rename the 
+#' variable XXXXX
+#' What would you like to rename XXXXX to?
+#' 
+#' (redisplay main menu until done)
+
+#' The result:
+#sprintf('inputdata <- c(\n %s\n);',paste0(c('dat01','dat02','dat03'),' = ',replicate(3,'"/tmp/2019-FA-TSCI-5050/data/example_data_pbc.csv"'),collapse='\n,'))
+
 #' 
 #' Check if a data-file exists
 #' 
-source('config.R',local=.checkenv);
-.inputdataset <- 'inputdata' %in% ls(.checkenv);
-.inputdataexists <- file.exists(.checkenv$inputdata);
+#source('config.R',local=.checkenv);
+#.inputdataset <- 'inputdata' %in% ls(.checkenv);
+#.inputdataexists <- file.exists(.checkenv$inputdata);
 #' TODO: talk user through setting up a config.R file
-#' 
-#' Find the submodules urls
-#' 
-.submodule <- getbrsub();
-.submodule <- paste0(.submodule['url'],'/archive/'
-                     ,if(.submodule['branch']=='') 'master' else {
-                       .submodule['branch']},'.zip');
-#' 
-#' Download the scripts submodule
-#' 
-utils::download.file(.submodule,'.scripts.zip');
-unzipto('.scripts.zip','scripts');
 #' 
 #' Prompt the user for the local data file.
 #' 
-done_fileread <- FALSE;
-.inputdata_names <- inputdata <- c();
-while(!done_fileread){
-  .filechosen <- try(file.choose());
-  if(!is(.filechosen,'try-error')){
-    inputdata <- c(inputdata,.filechosen);
-    .inputdata_names <- c(.inputdata_names,{
-      message("Please choose a short name for your data-source, preferably 3 characters or less and lower-case ")
-      readline("or type the 'return' key to accept a default name: ")});
-  };
-  done_fileread <- grepl('done'
-                         ,{message("\nType the 'return' key to add one more data file. ");
-                           tolower(
-                             readline(
-                               "If you are finished adding files, type 'DONE' and then the 'return' key. "))
-                           })};
-.inputdata_names <- gsub('^dat[[:digit:]]{0,2}$','',.inputdata_names);
-.inputdata_names <- gsub('^inputdata','',.inputdata_names);
-.inputdata_names[.inputdata_names==''] <- sprintf('dat%02d'
-                                                  ,seq_len(sum(.inputdata_names=='')));
-.inputdata_names <- make.names(.inputdata_names,unique=TRUE);
-.menu02 <- -1;
-while(.menu02 != 0){
-  .menu02 <- menu(paste(.inputdata_names,inputdata,sep=' = '),title="Here are the names modified for uniqueness and compatibility with R. Select any that you want to modify further or 0 to accept them and keep going. Please note that names beginning with 'dat' and ending with two digits will always get renumbered consecutively. Certain names that are known to interfere with these scripts will also get replaced with default values.");
-  if(.menu02>0){
-    .newname <- readline(paste0('Please type in the new name for '
-                                ,.inputdata_names[.menu02]
-                                ," or the 'enter' key for a default: "));
-    .inputdata_names[.menu02] <- .newname;
-    .inputdata_names <- gsub('^dat[[:digit:]]{0,2}$','',.inputdata_names);
-    .inputdata_names <- gsub('^inputdata','',.inputdata_names);
-    .inputdata_names[.inputdata_names==''] <- sprintf('dat%02d'
-                                                      ,seq_len(sum(
-                                                        .inputdata_names=='')));
-    .inputdata_names <- make.names(.inputdata_names,unique=TRUE);
-  }
-}
+# done_fileread <- FALSE;
+# .inputdata_names <- inputdata <- c();
+# while(!done_fileread){
+#   .filechosen <- try(file.choose());
+#   if(!is(.filechosen,'try-error')){
+#     inputdata <- c(inputdata,.filechosen);
+#     .inputdata_names <- c(.inputdata_names,{
+#       message("Please choose a short name for your data-source, preferably 3 characters or less and lower-case ")
+#       readline("or type the 'return' key to accept a default name: ")});
+#   };
+#   done_fileread <- grepl('done'
+#                          ,{message("\nType the 'return' key to add one more data file. ");
+#                            tolower(
+#                              readline(
+#                                "If you are finished adding files, type 'DONE' and then the 'return' key. "))
+#                            })};
+# .inputdata_names <- gsub('^dat[[:digit:]]{0,2}$','',.inputdata_names);
+# .inputdata_names <- gsub('^inputdata','',.inputdata_names);
+# .inputdata_names[.inputdata_names==''] <- sprintf('dat%02d'
+#                                                   ,seq_len(sum(.inputdata_names=='')));
+# .inputdata_names <- make.names(.inputdata_names,unique=TRUE);
+# .menu02 <- -1;
+# while(.menu02 != 0){
+#   .menu02 <- menu(paste(.inputdata_names,inputdata,sep=' = '),title="Here are the names modified for uniqueness and compatibility with R. Select any that you want to modify further or 0 to accept them and keep going. Please note that names beginning with 'dat' and ending with two digits will always get renumbered consecutively. Certain names that are known to interfere with these scripts will also get replaced with default values.");
+#   if(.menu02>0){
+#     .newname <- readline(paste0('Please type in the new name for '
+#                                 ,.inputdata_names[.menu02]
+#                                 ," or the 'enter' key for a default: "));
+#     .inputdata_names[.menu02] <- .newname;
+#     .inputdata_names <- gsub('^dat[[:digit:]]{0,2}$','',.inputdata_names);
+#     .inputdata_names <- gsub('^inputdata','',.inputdata_names);
+#     .inputdata_names[.inputdata_names==''] <- sprintf('dat%02d'
+#                                                       ,seq_len(sum(
+#                                                         .inputdata_names=='')));
+#     .inputdata_names <- make.names(.inputdata_names,unique=TRUE);
+#   }
+# }
 
 #' Update hooks.
 if(file.exists('scripts/quickstart_patch.R')) source('scripts/quickstart_patch.R');

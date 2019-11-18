4b8ddddf321c6b1c4ee449e31e7b4fad38dceff8
Alex Bokov <bokov@uthscsa.edu>
2019-11-18 16:35:23 -0600

    quickstart.R; bugfix; .menu0 instead of .menu01

--- quickstart.R
+++ quickstart.R
@@ -10,7 +10,7 @@ may take a while.
 You are running this at your own risk and with no warranty whatsoever.');
 .menu01 <- if(!interactive()) 3 else -1;
 if(file.exists('.auto.menu01.R')).menu01 <- source('.auto.menu01.R')$value;
-if(.menu01 == -1){
+if(.menu01 ==  -1){
   .menu01 <- menu(c('This is what I expected, go ahead.'
                     ,'Go ahead, but first create an empty directory.'
                     ,'Stop this script without making any changes to my computer.'

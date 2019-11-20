38069de30de721ccdbefd473a79ca67322a8cf20
Alex Bokov <bokov@uthscsa.edu>
2019-11-20 14:19:11 -0600

    quickstart.R; deploy; Added 'gitbootstrap()' function which insures availability of 'devtools' and then installs any needed github dependencies. Also a 'clean_slate()' function which is actually a wrapper that calls 'gitbootstrap()' and then 'tidbits:::clean_slate()'. Also use 'gitbootstrap()' in the script itself. Removed the menu tree dealing with creating a new directory. Finished the confmainloop which gathers information needed to edit the config files.

--- quickstart.R
+++ quickstart.R
@@ -12,12 +12,11 @@ You are running this at your own risk and with no warranty whatsoever.');
 if(file.exists('.auto.menu01.R')).menu01 <- source('.auto.menu01.R')$value;
 if(.menu01 ==  -1){
   .menu01 <- menu(c('This is what I expected, go ahead.'
-                    ,'Go ahead, but first create an empty directory.'
                     ,'Stop this script without making any changes to my computer.'
                     ))};
 
 
-if(!.menu01 %in% 1:2) stop('
+if(.menu01 != 1) stop('
 
 No problem, better safe than sorry!
 
@@ -25,27 +24,24 @@ Please read the comments in this script to understand what it does, and make
 sure you have backups of all the files in the directory where you run this 
 script (or that you run it in an empty directory). Then, feel free to come back
 and try this at a later time.');
-if(.menu01 == 2){
-  message("
 
-We will create an empty directory and work in there. Please give it a name. All
-spaces and punctuation marks except '_' will be removed. If a directory or file 
-by that name already exists, it will be renamed with the '.backup' prefix added 
-to it.
-")
-  .setdir <- readline('Please give a name for the directory to create: ');
-  .setdir <- gsub('[^[:alnum:]_]','',.setdir);
-  message('Your directory will be named ',.setdir);
-  if(file.exists(.setdir)){
-    file.rename(.setdir,paste0(.setdir,'.backup'));
-    message('An existing directory named ',.setdir,' has been backed up to '
-            ,.setdir,'.backup');
-    };
-  dir.create(.setdir);
-  setwd(.setdir);
-  message('From now on we are in the ',.setdir
-          ,' directory. Continuing installation.');
+gitbootstrap <- function(gitrepos=list(trailR=list(repo='bokov/trailR'
+                                                   ,ref='integration')
+                                       ,tidbits=list(repo='bokov/tidbits'
+                                                     ,ref='integration')
+                                       ,rio=list(repo='bokov/rio'
+                                                 ,ref='master'))
+                         ,instreqs=c()){
+  if(!require('devtools')){
+    install.packages('devtools',dependencies=TRUE
+                     ,repos=getOption('repos','https://cran.rstudio.com'))};
+  for(ii in names(gitrepos)){
+    do.call(devtools::install_github,gitrepos[[ii]]);
+    library(ii,character.only = TRUE)};
+  if(exists('instrequire')) instrequire(instreqs);
 }
+
+clean_slate <- function(...){gitbootstrap();tidbits:::clean_slate(...)};
 
 .templatepath <- 'https://github.com/bokov/2019-FA-TSCI-5050/archive/ft_simplescript.zip';
 .scriptspath <- 'https://github.com/bokov/ut-template/archive/master.zip';
@@ -62,13 +58,7 @@ if(file.exists('autoresponse.R')){
 #' Install needed libraries
 message('Installing needed packages and their dependencies.'
         ,'This may take a while, please be patient.');
-if(!require('devtools')){
-  install.packages('devtools',dependencies=TRUE
-                   ,repos=getOption('repos','https://cran.rstudio.com'))};
-devtools::install_github('bokov/trailR',ref='integration'); library(trailR);
-devtools::install_github('bokov/tidbits',ref='integration'); library(tidbits);
-devtools::install_github('bokov/rio',ref='master'); library(rio);
-instrequire('usethis');
+gitbootstrap(instreqs = 'usethis');
 
 #' 
 #' Copy down the latest version of the specified branch
@@ -93,11 +83,13 @@ if(file.exists(.localfns <- file.path(.scriptsinfo$path,'functions.R'))){
 options(browser=.oldoptions$browser); # restore the browser option
 #' For setting inputdata
 #' 
+
 .inputdata <- smartsetnames(smartfilechoose('data/example_data_pbc.csv'
                                        ,pop('confchfile')));
 # TODO: the same within loop, and grow with each cycle
 # TODO: print .inputdata as part of extramessage
 .confmain <- -1;
+# confmainloop ----
 while(.confmain<4 || length(.inputdata)==0){
   .confmain <- smartmenu(c( 'Select an additional file.'
                            ,'Change the name of a variable.'
@@ -113,12 +105,48 @@ while(.confmain<4 || length(.inputdata)==0){
                              ui_line(
                                '{ui_field(ii)}\t{ui_path(.inputdata[ii])}')}});
   switch(.confmain
+         # add a file
          ,.inputdata <- smartsetnames(c(.inputdata
                                         ,smartfilechoose('data/example_data_pbc.csv'
+                                                         ,ignorecancel = FALSE
                                                          ,auto=pop('confchfile')
                                                          )))
-         ,message('RENAME VAR')
-         ,message('REMOVE PATH'));
+         ,{ # rename a variable
+           .torename <- smartmenu(.inputdata
+                                  ,title='Which variable do you wish to rename?'
+                                  ,extramessage = {
+                                    ui_line(
+"After you enter in the name you want it will get modified for uniqueness, 
+compatibility with R and ease of typing. Names starting with {ui_code('dat')} 
+will be renumbered. Type {ui_code(0)} to cancel and go back to previous menu.")}
+                                  ,ignorezero = FALSE,batchmode=0
+                                  ,auto=pop('confchfile'));
+           if(.torename==0) next;
+           message(ui_line(
+             "\nIf you want a default name to be auto-assigned, type the 'enter' key"));
+           names(.inputdata)[.torename]<-smartreadline(
+             sprintf("Type in a new name for '%s': "
+                     ,names(.inputdata)[.torename])
+             ,auto=pop('confchfile'));
+           .inputdata <- smartsetnames(.inputdata);
+         }
+         ,{ # remove a file
+           .toremove <- smartmenu(.inputdata
+                                  ,title='Which file do you wish to un-select?'
+                                  ,extramessage = {ui_line(
+'The file you unselect will not be touched, just its data will not be imported 
+into this project. Type {ui_code(0)} to not select any files and return to the 
+previous menu.')}
+                                  ,ignorezero = FALSE, batchmode = 0
+                                  ,auto=pop('confchfile'));
+           if(.toremove==0) next;
+           .inputdata <- .inputdata[-.toremove];
+           if(length(.inputdata)==0){
+             message(ui_todo('There must be at least one data file.'));
+             .inputdata <- smartsetnames(smartfilechoose('data/example_data_pbc.csv'
+                                                         ,pop('confchfile')));
+           }
+           });
 }
 .userconfigdone <- FALSE;
 #' While !.userconfigdone

b91d80284cb9e141edbdc610637bc826083b7123
Alex Bokov <bokov@uthscsa.edu>
2019-11-21 20:16:02 -0600

    prep_deps.R; tweak; Further simplifying script.

--- prep_deps.R
+++ prep_deps.R
@@ -1,20 +1,21 @@
 #' ---
-#' title: "All the Files"
+#' title: "Load All Scriports"
 #' ---
 #' 
-# additional packages to install, if needed. If none needed, should be: ''
 .projpackages <- c()
-# name of this script
 .currentscript <- parent.frame(2)$ofile;
-# other scripts which need to run before this one. If none needed, shoule be: ''
 .deps <- c( 'dictionary.R','simdata.R'); 
-# load project-wide settings, including your personalized config.R
-.junk<-capture.output(source('./scripts/global.R',chdir=T,echo=F));
-# files not created by this script are in .origfiles
+#+ load_deps, echo=FALSE, messages=FALSE, warnings=FALSE
+.junk<-capture.output(source('./scripts/global.R',chdir=TRUE,echo=FALSE));
 .origfiles <- c();
+
+
 
 #' Normally this is where calculations should go, but this script is invoked for
 #' its side effects: triggering all the other scripts.
+
+
+
 
 save(file=paste0(.currentscript,'.rdata'),list=setdiff(ls(),.origfiles));
 #+ echo=FALSE

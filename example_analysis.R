915f3064c705018f7f3da9ac0017a3705332c03b
Alex Bokov <bokov@uthscsa.edu>
2019-12-06 19:43:52 -0600

    example_analysis.R,prep_deps.R; automation; 'example_analysis.R' is the scriport where actual models will be fit and plot will be plotted so now 'prep_deps.R' only needs to depend on it and 'simdata.R' in order to achieve full coverage, for now

---
+++ example_analysis.R
@@ -0,0 +1,28 @@
+#' ---
+#' title: "Example Analysis"
+#' ---
+#' 
+.projpackages <- c()
+.deps <- c( 'dictionary.R' ); 
+#+ load_deps, echo=FALSE, messages=FALSE, warnings=FALSE
+.junk<-capture.output(source('./scripts/global.R',chdir=TRUE,echo=FALSE));
+#.currentscript <- parent.frame(2)$ofile;
+.currentscript <- current_scriptname('example_analysis.R');
+.origfiles <- c();
+
+#' ### Choosing predictor and response variables
+#' 
+#' Replace `binary_outcome` and `numeric_outcome` below with vectors 
+#' containing one or more column names you actually want to serve as the 
+#' outcome variables
+binary_outcome <- choose_outcomes(dat01,c_safetf);
+numeric_outcome <- choose_outcomes(dat01,c_safenumeric);
+predictorvars <- choose_predictors(dat01,c_safe
+                                   ,exclude=c(binary_outcome,numeric_outcome));
+mainvars <- c(predictorvars,binary_outcome,numeric_outcome);
+
+
+
+save(file=paste0(.currentscript,'.rdata'),list=setdiff(ls(),.origfiles));
+#+ echo=FALSE
+c()
